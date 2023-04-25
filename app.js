3d5c9afd8dc4837aac3181bf5b5c2d5e1e48ac06
Angela <angelakulola4@gmail.com>
2023-04-25 23:38:26 +0300

    Updated HTML and CSS

--- app.js
+++ app.js
@@ -5,5 +5,22 @@
 //CAROUSEL 
 
 //PHOTO OF THE DAY.
+// Fetch the photo of the day from NASA APOD API
+// Fetch the photo of the day from NASA APOD API
+// Fetch the photo of the day from NASA APOD API
+fetch('https://api.nasa.gov/planetary/apod?api_key=1DF4WOFyAGA5G2hUd87vzhuyoEv9dGq7GiHBWKmW')
+  .then(response => response.json())
+  .then(data => {
+    // Update the HTML with the photo of the day and its description
+    const potdImage = document.getElementById('potd-image');
+    const potdTitle = document.getElementById('potd-title');
+    const potdDescription = document.getElementById('potd-description');
+    potdImage.src = data.url;
+    potdTitle.textContent = data.title;
+    potdDescription.textContent = data.explanation;
+  })
+  .catch(error => {
+    console.error(error);
+  });
 
 //SEARCH RESULTS.
